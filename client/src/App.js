905e8908af0cdb8d987b9ddacac093f6645e486a
Christopher Terrano <mterrano1@yahoo.com>
2022-08-21 00:26:03 -0500

    testing deployment

--- client/src/App.js
+++ client/src/App.js
@@ -17,7 +17,7 @@ function App() {
             target="_blank"
             rel="noopener noreferrer"
           >
-            Learn React
+            Hope this works!
           </a>
           <Link to="/">Home</Link>
           <Link to="/otherpage">Other Page</Link>
