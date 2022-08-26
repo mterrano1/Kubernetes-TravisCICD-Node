fea5f75b3103bcf797e8aa6fe3de55c9c749048a
Christopher Terrano <mterrano1@yahoo.com>
2022-08-26 11:13:37 -0500

    add k8s

--- client/src/App.js
+++ client/src/App.js
@@ -17,7 +17,7 @@ function App() {
             target="_blank"
             rel="noopener noreferrer"
           >
-            Hope this works!
+            Fib Calculator version 2
           </a>
           <Link to="/">Home</Link>
           <Link to="/otherpage">Other Page</Link>
